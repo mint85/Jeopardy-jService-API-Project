038ec5d3a415f901dcb80d99fff3b45c1eb6ae2c
Allen Jones <allen.jones@gmail.com>
2021-12-13 17:09:49 +0000

    added js event listener that retrieves a new set of data (category, clue, and answer) when user clicks the 'get clue' button.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // variables
-const URL = "https://jservice.io/api/random"
+const URL = "https://jservice.io/api/random";
 
 
 // element references
-
+const $category = $('#category')
+const $getClue = $('#getClue');
+const $getAnswer = $('#getAnswer');
 
 // event listeners
-
+$getClue.on('click', getJeopardyData);
 
 
 // functions
 function getJeopardyData() {
     // evt.preventDefault();
 
-    $.ajax(URL).then(function(data) {
+    $.ajax(URL).then(function (data) {
         // render(data);
         console.log(data);
         console.log(data[0].category.title);
         console.log(data[0].question);
         console.log(data[0].answer);
-        
-    })
+    });
 }
 
 getJeopardyData();
