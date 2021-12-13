d614f2eb92f09cb723cb6ec42d5ea79efadbfb4b
Allen Jones <allen.jones@gmail.com>
2021-12-13 16:30:10 +0000

    added js code to define url, function getJeopardyData that uses ajax to pull information from jservice api, code to test data retrieved in console. added category heading in index.html

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("JS is fueled and ready to blast off!!")
+// variables
+const URL = "https://jservice.io/api/random"
+
+
+// element references
+
+
+// event listeners
+
+
+
+// functions
+function getJeopardyData() {
+    // evt.preventDefault();
+
+    $.ajax(URL).then(function(data) {
+        // render(data);
+        console.log(data);
+        console.log(data[0].category.title);
+        console.log(data[0].question);
+        console.log(data[0].answer);
+        
+    })
+}
+
+getJeopardyData();
