003b5ee703926e2d06f1854baf7eeb1d995c579a
Allen Jones <allen.jones@gmail.com>
2021-12-13 20:41:00 +0000

    created 'reset' button that clears all fields when clicked

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const URL = "https://jservice.io/api/random";
 const $category = $('#category')
 const $getClue = $('#getClue');
 const $getAnswer = $('#getAnswer');
+const $reset = $('#reset');
 
 // event listeners
 $getClue.on('click', getJeopardyData);
 $getAnswer.on('click', displayAnswer);
+$reset.on('click', resetFields);
 
 // functions
 function getJeopardyData(evt) {
@@ -17,25 +19,31 @@ function getJeopardyData(evt) {
 
     $.ajax(URL).then(function (data) {
         displayNewClue(data);
+        // displayAnswer(data);
         console.log(data);
-        console.log(data[0].category.title);
-        console.log(data[0].question);
-        console.log(data[0].answer);
-    }, function(error) {
+        // console.log(data[0].category.title);
+        // console.log(data[0].question);
+        // console.log(data[0].answer);
+    },     
+        function(error) {
         console.log('something went wrong');
         console.log(error);
-    });
+        });
 }
 
 function displayNewClue(clueData) {
-    $('#category').html(`<p>${clueData[0].category.title}</p>`);
+    $('#category').html(`Category: ${clueData[0].category.title}`);
     $('#clue').html(`Clue: ${clueData[0].question}`);
 }
 
+// this does not display the answer. I don't think it can access
+// the d
 function displayAnswer(clueData) {
-    
+    $('#answer').html(`Answer: ${clueData[0].answer}`);
 }
 
 function resetFields() {
-
+    $('#category').html(`Category:`);
+    $('#clue').html(`Clue:`);
+    $('#answer').html(`Answer:`);
 }
