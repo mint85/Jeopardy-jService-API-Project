5b157cc9c66979276264cbc1159fc05c594c699b
Allen Jones <allen.jones@gmail.com>
2021-12-13 18:00:04 +0000

    added javascript code to display the clue category and the clue itself when the 'get clue' button is clicked

--- script.js
+++ script.js
@@ -9,19 +9,33 @@ const $getAnswer = $('#getAnswer');
 
 // event listeners
 $getClue.on('click', getJeopardyData);
-
+$getAnswer.on('click', displayAnswer);
 
 // functions
-function getJeopardyData() {
-    // evt.preventDefault();
+function getJeopardyData(evt) {
+    evt.preventDefault();
 
     $.ajax(URL).then(function (data) {
-        // render(data);
+        displayNewClue(data);
         console.log(data);
         console.log(data[0].category.title);
         console.log(data[0].question);
         console.log(data[0].answer);
+    }, function(error) {
+        console.log('something went wrong');
+        console.log(error);
     });
 }
 
-getJeopardyData();
+function displayNewClue(clueData) {
+    $('#category').html(`<p>${clueData[0].category.title}</p>`);
+    $('#clue').html(`Clue: ${clueData[0].question}`);
+}
+
+function displayAnswer(clueData) {
+    
+}
+
+function resetFields() {
+
+}
