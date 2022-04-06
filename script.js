92839ee9f480121a05f8319bd60980d3f8a80aa9
Allen Jones <allen.jones@gmail.com>
2022-04-06 21:18:15 +0000

    incorporates reset button functionality into get clue button. reorganizes script.js file and adds new/more comments

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-// variables
+// VARIABLES
 const URL = "https://jservice.io/api/random";
 const ajaxResult = [];
 
-// element references
+
+// ELEMENT REFERENCES
 const $category = $("#category");
 const $getClue = $("#getClue");
 const $getAnswer = $("#getAnswer");
 const $reset = $("#reset");
 const $titleText = $("#titleText");
 
-// event listeners
+
+// EVENT LISTENERS
 $getClue.on("click", getJeopardyData);
 $getAnswer.on("click", displayAnswer);
 $reset.on("click", resetFields);
 
 
-// functions
+// FUNCTIONS
+
+// Resets the Category, Clue, and Answer fields on the page. Also resets the ajaxResult array to empty.
+function resetFields() {
+    $("#category").html(`<u>Category</u> :`);
+    $("#clue").html(`<u>Clue</u> :`);
+    $("#answer").html(`<u>Answer</u> :`);
+    ajaxResult.length = 0; 
+};
+
+// Calls resetFields function. 
+// Retrieves new random Jeopardy clue data from jService API.
+// Stores clue data in ajaxResult array.
+// Calls displayNewClue function.
 function getJeopardyData(evt) {
     evt.preventDefault();
+    resetFields();
     $.ajax(URL).then(function (data) {
             // put all received data in ajaxResult array for later use
             ajaxResult.push(data);
@@ -31,28 +47,21 @@ function getJeopardyData(evt) {
         function (error) {
             console.log("something went wrong");
             console.log(error);
-        }
-    );
-}
+        });
+};
 
+// Retrieves the category and clue data and displays it on the page.
 function displayNewClue(clueData) {
     $("#category").html(`<u>Category</u> : ${clueData[0].category.title}`);
     $("#clue").html(`<u>Clue</u> : ${clueData[0].question}`);
-}
+};
 
-    // Access the answer from the ajaxResult array I created
+// Access the answer from the ajaxResult array and displays it on the page.
 function displayAnswer() {
     $("#answer").html(`<u>Answer</u> : ${ajaxResult[(0, 0)][0].answer}`);
-}
+};
 
-function resetFields() {
-    $("#category").html(`<u>Category</u> :`);
-    $("#clue").html(`<u>Clue</u> :`);
-    $("#answer").html(`<u>Answer</u> :`);
-    // resets ajaxResult array to empty, ready to recieve
-    // another set of data
-    ajaxResult.length = 0;
-}
+
 
 // possible event listener to fade in parts of the page on initial page load?
 // window.addEventListener('DOMcontentLoaded', function() {
